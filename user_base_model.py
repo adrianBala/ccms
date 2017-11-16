943b049511994dd237ede88df084518b75a91f52
Joanna Baran <joannabaran56@gmail.com>
2017-11-16 16:51:20 +0100

    Add user base constructor

--- user_base_model.py
+++ user_base_model.py
@@ -1,2 +1,8 @@
 class UserBase():
-    pass
+
+    def __init__(self, name, surname, email, phone_number, password):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.phone_number = phone_number
+        self.password = password
