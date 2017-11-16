69155abda952c5b66833153bc9bd02497c30c72d
Joanna Baran <joannabaran56@gmail.com>
2017-11-16 17:15:28 +0100

    Add student constructor in student model

--- student_model.py
+++ student_model.py
@@ -1,2 +1,7 @@
-class Student():
-    pass
+from user_base_model import UserBase
+
+
+class Student(UserBase):
+
+    def __init__(self, name, surname, email, phone_number, password):
+        super().__init(name, surname, email, phone_nubmer, password)
