52310fd608d00f0f61ce7041a2571c826a3c3f03
Sebastian <sebastian.halinski@gmail.com>
2017-11-16 15:53:06 +0100

    Implement user base controler

--- user_base_controller.py
+++ user_base_controller.py
@@ -1,2 +1,30 @@
+from user_base_model import UserBase
+from user_base_dao import UserBaseDao
+from user_base_view import UserBaseView
+from user_base_container import UserBaseContainer
+
 class UserBaseController():
-    pass
+
+    def __init__(self):
+        self.view = UserBaseView()
+        self.dao = UserBaseDao()
+        self.container = UserBaseContainer()
+
+    def validate_password(self, email, password, user_email, user_password):
+        return email == user_email and password == user_password
+
+    def sign_in(self):
+        user_email = self.view.get_email()
+        user_password = self.view.get_password()
+
+        login_info = self.dao.import_login_info()
+        self.container.set_login_info(login_info)
+
+        for row in self.container.get_login_info():
+            email, password, status = row
+            if user_email == email:
+                if self.validate_password(email, password, user_email, user_password):
+                    return email, status
+                else:
+                    self.view.display_text('Access denied!')
+                break
