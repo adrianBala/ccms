64307987e390dc486fdd560aa49a0ef85e25cead
Sebastian <sebastian.halinski@gmail.com>
2017-11-16 15:51:23 +0100

    Implement user base container

--- user_base_container.py
+++ user_base_container.py
@@ -1,2 +1,13 @@
+from user_base_dao import UserBaseDao
+
+
 class UserBaseContainer():
-    pass
+
+    def __init__(self):
+        self.login_info = []
+
+    def set_login_info(self, login_info):
+        self.login_info = login_info
+
+    def get_login_info(self):
+        return self.login_info
