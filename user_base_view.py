58460e8b22e52075487384719fe1e2163c261188
Justyna Fornalik <jfornalik@gmail.com>
2017-11-16 14:30:02 +0100

    Add a method to get user e-mail and password

--- user_base_view.py
+++ user_base_view.py
@@ -1,2 +1,8 @@
 class UserBaseView():
-    pass
+    
+    def get_email(self):
+        user_email = input('Enter e-mail: ')
+        return user_email
+
+    def get_password(self):
+        user_password = input('Enter password: ')
