262ca01fa66768d933c4960db04f0e6e5f302ce4
Adrian Bala <aadrianbala@gmail.com>
2017-11-16 20:28:46 +0100

    Add mentors to container

--- mentor_container.py
+++ mentor_container.py
@@ -6,5 +6,8 @@ class MentorContainer():
     def get_mentors(self):
         return self.mentors
 
+    def set_mentors(self, mentors):
+        self.mentors = mentors
+
     def add_mentor(self, mentor):
         self.mentors.append(mentor)
