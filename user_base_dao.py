bd33fae1957de23a151ee8f11eff48dc48e03bd0
Sebastian <sebastian.halinski@gmail.com>
2017-11-16 15:48:48 +0100

    Implement user base dao

--- user_base_dao.py
+++ user_base_dao.py
@@ -1,2 +1,14 @@
+import csv
+
+
 class UserBaseDao():
-    pass
+    login_file = 'login.csv'
+
+    def import_login_info(self):
+        with open(self.login_file, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            login_info = []
+            for row in reader:
+                uid, login, password = row
+                login_info.append(row)
+            return login_info
