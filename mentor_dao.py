5422a959cb339b6cb241eedaa836d31e10c483be
Adrian Bala <aadrianbala@gmail.com>
2017-11-16 17:53:56 +0100

    Add creating mentor and upgrade importing mentors

--- mentor_dao.py
+++ mentor_dao.py
@@ -5,10 +5,16 @@ class MentorDao():
 
     mentors_file = 'mentors.csv'
 
+    def create_mentor(self, row):
+        name, surname, email, phone, password = row
+        mentor = MentorModel(name, surname, email, phone, password)
+        return mentor
+
     def import_mentors(self):
         mentors = []
         with open(self.mentors_file, "r") as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                mentors.append(row)
+                mentor = create_mentor(row)
+                mentors.append(mentor)
         return mentors
