420df7a69891073d5908bc307d3088f60095e5b8
Justyna Fornalik <jfornalik@gmail.com>
2017-11-16 16:43:00 +0100

    Add get_students_data_from_user method in MentorView class

--- mentor_view.py
+++ mentor_view.py
@@ -17,4 +17,14 @@ class MentorView():
         menu_option = input("Enter a number: ")
         return menu_option
 
+    def get_students_data_from_user(self):
+        students_name = input("Enter student's name: ")
+        students_surname = input("Enter student's surname: ")
+        students_email = input("Enter student's e-mail: ")
+        students_phone = input("Enter student's phone: ")
+        students_password = input("Enter student's password: ")
+        students_class = input("Enter student's class: ")
+
+        students_data = (students_name, students_surname, students_email, students_phone, students_class)
+        return students_data  # zwraca tuplę
     
