5d4af0b53c8a114e08896cdcd336f3fa9fe8b0cd
Adrian Bala <aadrianbala@gmail.com>
2017-11-16 17:21:13 +0100

    Add import list of student and get list of student from student dao to manager controller

--- manager_controller.py
+++ manager_controller.py
@@ -6,6 +6,12 @@ class ManagerController():
     def __init__(self):
         self.view = ManagerView()
         self.mentor_dao = MentorDao()
+        self.student_dao = StudentDao()
 
     def get_list_of mentors(self):
         return self.mentor_dao.import_mentors()
+
+    def get_list_of students(self):
+        return self.student_dao.import_students()
+
+    
--- student_dao.py
+++ student_dao.py
@@ -1,2 +1,13 @@
+import csv
+
 class StudentDao():
-    pass
+
+    students_file = 'students.csv'
+
+    def import_students(self):
+        students = []
+        with open(self.students_file, "r") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                students.append(row)
+        return students
