054c453d80353880bcee00616001441a0e9b993f
Justyna Fornalik <jfornalik@gmail.com>
2017-11-16 16:30:25 +0100

    Add display_mentors_menu method in MentorView class

--- mentor_view.py
+++ mentor_view.py
@@ -1,2 +1,16 @@
 class MentorView():
-    pass
+    
+    def display_mentors_menu(self):
+        options = ('(1) List students',
+                   '(2) Add student',
+                   '(3) Remove student',
+                   '(4) Edit student',
+                   '(5) Add assignment',
+                   '(6) Grade assignment',
+                   '(7) Check attendance',
+                   '(0) Exit CcMS')
+        for option in options:
+            print(option)
+        print('What would you like to do: ')
+
+    
