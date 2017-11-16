1a713096dcae0e90d67609173067ddb3d2a231f0
Adrian Bala <aadrianbala@gmail.com>
2017-11-16 19:18:00 +0100

    Fix importing and creating mentor

--- mentor_dao.py
+++ mentor_dao.py
@@ -5,8 +5,7 @@ class MentorDao():
 
     mentors_file = 'mentors.csv'
 
-    def create_mentor(self, row):
-        name, surname, email, phone, password = row
+    def create_mentor(self, name, surname, email, phone, password):
         mentor = MentorModel(name, surname, email, phone, password)
         return mentor
 
@@ -15,6 +14,17 @@ class MentorDao():
         with open(self.mentors_file, "r") as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                mentor = create_mentor(row)
+                mentor = create_mentor(*row)
                 mentors.append(mentor)
         return mentors
+
+    def export_mentors(self, mentors):
+        with open(self.mentors_file, "w") as csvfile:
+            writer = csv.writer(csvfile)
+            for mentor in mentors:
+                name = mentor.get_name()
+                surname = mentor.get_surname()
+                email = mentor.get_email()
+                phone = mentor.get_phone_number()
+                password = mentor.get_password()
+                writer.writerow([name, surname, email, phone, password])
