210a6f5ddf15e6f051ff358bc0bb653214448a3f
Adrian Bala <aadrianbala@gmail.com>
2017-11-16 19:26:42 +0100

    Add adding mentor and change get list of mentors feature

--- manager_controller.py
+++ manager_controller.py
@@ -1,5 +1,7 @@
+from manager_view import ManagerView
 from mentor_dao import MentorDao
 from student_dao import StudentDao
+from mentor_container import MentorContainer
 
 class ManagerController():
 
@@ -8,10 +10,20 @@ class ManagerController():
         self.mentor_dao = MentorDao()
         self.student_dao = StudentDao()
 
-    def get_list_of mentors(self):
-        return self.mentor_dao.import_mentors()
-
     def get_list_of students(self):
         return self.student_dao.import_students()
 
-    
+    def get_mentor_container(self):
+        try:
+            return self.mentor_container
+        except AttributeError:
+            self.mentor_container = MentorContainer()
+            self.mentor_container.import_mentors()
+        return self.mentor_container
+
+    def add_mentor(self):
+        mentor_data = self.view.get_mentors_data()
+        mentor = self.mentor_dao.create_mentor(mentor_data)
+        self.get_mentor_container.add_mentor(mentor)
+        mentors = self.get_mentor_container().get_mentors()
+        self.mentor_dao.export_mentors(mentors)
