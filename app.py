2dd9982ac4d76272534976623bb5ce3ad5a75e4a
Sebastian <sebastian.halinski@gmail.com>
2017-11-16 14:48:12 +0100

    Add main app class

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
+from root_controller import RootController
+
+
 class App():
-	pass
+
+    def __init__(self):
+        self.root_controller = RootController()
+
+    def main(self):
+        self.root_controller.start()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.main()
