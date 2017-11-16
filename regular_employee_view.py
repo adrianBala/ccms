8c1f83fbe5f6df26428830678f5bccdb5c63e249
Justyna Fornalik <jfornalik@gmail.com>
2017-11-16 17:24:47 +0100

    Add display_employees_menu method in RegularEmployeeView class

--- regular_employee_view.py
+++ regular_employee_view.py
@@ -1,2 +1,12 @@
 class RegularEmployeeView():
-    pass
+
+    def display_employee_menu(self):
+        options = ('(1) List students',
+                   '(0) Exit CcMS')
+        for option in options:
+            print(option)
+        print('What would you like to do: ')
+        
+
+    
+
