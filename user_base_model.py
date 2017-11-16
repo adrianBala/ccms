28aa7dfb7d791030cc564d80418935498db96448
Joanna Baran <joannabaran56@gmail.com>
2017-11-16 20:26:48 +0100

    Add getters and str in user base model

--- user_base_model.py
+++ user_base_model.py
@@ -7,6 +7,24 @@ class UserBase():
         self.phone_number = phone_number
         self.password = password
 
+    def __str__(self):
+        return '{} {} {} {}'.format(self.name, self.surname, self.email, self.phone_number)
+
+    def get_name(self):
+        return self.name
+
+    def get_surname(self):
+        return self.surname
+
+    def get_email(self):
+        return self.email
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_password(self):
+        return self.password
+
     def change_email(self, new_email):
         self.email = new_email
 
