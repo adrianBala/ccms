17ee211d65970be35e6e00a2782cc4f8798943d2
Justyna Fornalik <jfornalik@gmail.com>
2017-11-16 14:45:15 +0100

    Add display_managers_menu method in ManagerView class

--- manager_view.py
+++ manager_view.py
@@ -1,2 +1,14 @@
 class ManagerView():
-    pass
+    
+    def display_managers_menu(self):
+        options = ('(1) List mentors',
+                   '(2) List students',
+                   '(3) Add mentor',
+                   '(4) Remove mentor',
+                   '(5) Edit mentor',
+                   '(0) Exit CcMS')
+        
+
+        print(options)
+        print("What would you like to do?")
+        
