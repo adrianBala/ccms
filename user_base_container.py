33ca4cb7f3b21c4f5d057f8dcb83cb98ba7dcd80
Joanna Baran <joannabaran56@gmail.com>
2017-11-28 22:45:26 +0100

    Change method order in user base container

--- user_base_container.py
+++ user_base_container.py
@@ -6,11 +6,11 @@ class UserBaseContainer():
     def __init__(self):
         self.login_info = []
 
-    def set_login_info(self, login_info):
-        self.login_info = login_info
-
     def get_login_info(self):
         return self.login_info
+
+    def set_login_info(self, login_info):
+        self.login_info = login_info
 
     def add_user(self, login, password, status):
         self.login_info.append([login, password, status])
