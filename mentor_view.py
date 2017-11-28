5a81182a8b915c3766cb723f3c0af96f189a1f6b
Marcin Wierciak <marcin.f.w@gmail.com>
2017-11-28 14:49:04 +0100

    Secure input for mentor_view module

--- mentor_view.py
+++ mentor_view.py
@@ -1,5 +1,7 @@
+import string
+
 class MentorView():
-    
+
     def display_mentors_menu(self):
         options = ('(1) List students',
                    '(2) Add student',
@@ -18,17 +20,67 @@ class MentorView():
         return menu_option
 
     def get_students_data_from_user(self):
-        students_name = input("Enter student's name: ")
-        students_surname = input("Enter student's surname: ")
-        students_email = input("Enter student's e-mail: ")
-        students_phone = input("Enter student's phone: ")
+        students_name = self.get_name_or_surname('name')
+        students_surname = self.get_name_or_surname('surname')
+        students_email = self.get_students_email()
+        students_phone = self.get_tel_number()
         students_password = input("Enter student's password: ")
-        students_class = input("Enter student's class: ")
+        students_class =
 
         students_data = (students_name, students_surname, students_email, students_phone, students_password, students_class)
         return students_data  # zwraca tuplę
-        
+
+    def get_name_or_surname(self, name_or_surname):
+        while True:
+            user_input = input("Enter student's {}: ".format(name_or_surname))
+            if len(user_input) != 0:
+                if user_input.isspace():
+                    print('\nWrong input. Enter at least one "nonspace" letter.')
+
+                    continue
+
+                return user_input
+
+            print('\nWrong input. Enter at least one character.')
+
     def get_students_email(self):
-        students_email = input("Enter student's e-mail: ")
-        return students_email
-    
+        valid_chars = string.ascii_lowercase + string.digits + '.'
+        valid_chars_plus = string.ascii_lowercase + string.digits + '_.-'
+        while True:
+            email = input("Enter student's e-mail: ")
+            if len(email) => 5 and email.isspace() == False and '@' in email and '.' in email:
+                chars_to_monkey = email.split('@')[0]
+                chars_after_monkey = email.split('@')[1]
+                if chars_after_monkey.count('.') > 0:
+                    chars_before_last_dot = chars_after_monkey.split('.')[0]
+                    chars_after_last_dot = chars_after_monkey.split('.')[1]
+                    if len(chars_before_last_dot) => 1 and len(chars_after_last_dot) => 1:
+                        if all(x in valid_chars_plus for x in chars_to_monkey):
+                            if all(x in valid_chars for x in chars_after_monkey):
+
+                                return email
+
+            print('\nWrong input. Enter email in format: "x@x.x".')
+
+    def get_tel_number(self):
+        valid_chars = string.digits + '+()-'
+        while True:
+            telnumber = input("Enter mentor's phone: ")
+            if all(x in valid_chars for x in telnumber):
+                if sum(c.isdigit() for c in telnumber) >= 9:
+
+                    return telnumber
+
+                print('\nWrong input. Enter at least 9 digits.')
+                continue
+
+            print('\nWrong input. Enter digits and "+()-".')
+
+    def get_students_class(self):
+        letters = string.uppercase
+        digits = string.digits
+        students_class = input("Enter student's class: ")
+        if len(students_class) == 2 and students_class[0].isdigit() and students_class[1].isalpha():
+            return students_class[0] + students_class[1].upper()
+
+        print('\nWrong input. Enter class in format: "XY", where "X" is a single digit and "Y" is a single letter.')
