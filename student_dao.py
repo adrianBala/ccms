697bd95db4598b06b8d1f8a80a4b83e9fca6cdd1
Sebastian <sebastian.halinski@gmail.com>
2017-11-28 11:40:40 +0100

    Add export method in student dao

--- student_dao.py
+++ student_dao.py
@@ -19,3 +19,16 @@ class StudentDao():
                 else:
                     students[class_name] = [student]
         return students
+
+    def export_students(self, students):
+        with open(self.students_file, "w") as csvfile:
+            writer = csv.writer(csvfile)
+            for student_list in students.values():
+                for student in students_list:
+                    name = student.get_name()
+                    surname = student.get_surname()
+                    email = student.get_email()
+                    phone = student.get_phone_number()
+                    password = student.get_password()
+                    class_name = student.get_class_name()
+                    writer.writerow([name, surname, email, phone, password, class_name])
