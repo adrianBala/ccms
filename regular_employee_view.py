7f3594823d67f3cef9eafaa1781f1f81f5b02e63
Joanna Baran <joannabaran56@gmail.com>
2017-11-28 23:20:13 +0100

    Modify method get menu option and change method order in reg employee view

--- regular_employee_view.py
+++ regular_employee_view.py
@@ -14,14 +14,18 @@ class RegularEmployeeView():
             print('\t' + option)
 
     def get_menu_option(self):
+        correct_choices = ('0', '1')
         menu_option = input("Option: ")
+        while menu_option not in correct_choices:
+            print('Wrong input!')
+            menu_option = input("Option: ")
         return menu_option
-
-    def display_goodbye_message(self):
-        print('\nGoodbye!')
 
     def display_list(self, collection):
         my_table = PrettyTable(["No.", "NAME", "SURNAME", "E-MAIL", "PHONE", "CLASS"])
         for user in collection:
             my_table.add_row(user)
         print(my_table)
+
+    def display_goodbye_message(self):
+        print('\nGoodbye!')
