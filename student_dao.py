0d4773d4e7eae2d3d6331398206b8aef146588ef
Sebastian <sebastian.halinski@gmail.com>
2017-11-28 11:36:44 +0100

    Modify import students method to return dict in student dao

--- student_dao.py
+++ student_dao.py
@@ -6,10 +6,16 @@ class StudentDao():
     students_file = 'students.csv'
 
     def import_students(self):
-        students = []
+        students = {}
+        class_index = -1
+
         with open(self.students_file, "r") as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
+                class_name = row[class_index]
                 student = Student(*row)
-                students.append(student)
+                if class_name in students:
+                    students[class_name].append(student)
+                else:
+                    students[class_name] = [student]
         return students
