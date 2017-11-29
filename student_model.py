451c99cd382c6a364e6b41e36b3aef89b514be5d
Sebastian <sebastian.halinski@gmail.com>
2017-11-29 16:02:30 +0100

    Add get avarange attendance method in student model

--- student_model.py
+++ student_model.py
@@ -19,11 +19,16 @@ class Student(UserBase):
 
     def set_attendance(self, student_attendance):
         if student_attendance == 'persence':
-            self.attendance = self.attendance + "1"
+            self.attendance = self.attendance + '1'
         elif student_attendance == 'late':
-            self.attendance = self.attendance + "2"
+            self.attendance = self.attendance + '2'
         elif student_attendance == 'absence':
-            self.attendance = self.attendance + "0"
+            self.attendance = self.attendance + '0'
 
-    # def get_avarage_attendance(self):
-    #     return float((self.attendance[0] / self.attendance[1]) * 100)
+    def get_avarage_attendance(self):
+        attendance_data = self.attendance
+        full_attendance = len(self.attendance)
+        current_attendance = self.attendance.count('1')
+        attendance_but_late = self.attendance.count('2')
+        attendance_vlaue = ((current_attendance + attendance_but_late * 0.8) / full_attendance) * 100
+        return round(attendance_vlaue, 2)
