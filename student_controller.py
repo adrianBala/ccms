80507851bfc9cceee66779da1312982b7db06068
Adrian Bala <aadrianbala@gmail.com>
2017-11-29 21:34:30 +0100

    Add displaying grades in student controller

--- student_controller.py
+++ student_controller.py
@@ -1,15 +1,40 @@
 import os
 
+from assignment_container import AssignmentContainer
+from assignment_dao import AssignmentDao
+from student_container import StudentContainer
 from student_view import StudentView
 
 
 class StudentController():
 
-    def __init__(self):
+    def __init__(self, email=''):
         self.view = StudentView()
+        self.email = email
+        self.assignment_dao = AssignmentDao()
+
+    def get_assignment_container(self):
+        try:
+            return self.assignment_container
+        except AttributeError:
+            self.assignment_container = AssignmentContainer()
+            assignments = self.assignment_dao.import_assignments()
+            self.assignment_container.set_assignments(assignments)
+        return self.assignment_container
+
+    def list_student_assignments(self):
+        assignments_data_collection = []
+
+        student_assignments = self.get_assignment_container().get_assignments_of_student(self.email)
+        for count, assignment in enumerate(student_assignments, 1):
+            assignment_data = [count, assignment.get_name(), assignment.get_url(),
+                               assignment.get_grade()]
+            assignments_data_collection.append(assignment_data)
+
+        return assignments_data_collection
 
     def view_grades(self):
-        pass
+        self.view.display_grades(self.list_student_assignments())
 
     def submit_assignment(self):
         pass
