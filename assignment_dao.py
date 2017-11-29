9b2b945d20f96ba674ce07b84c173a9fb0f6b1dd
Joanna Baran <joannabaran56@gmail.com>
2017-11-29 11:45:26 +0100

    Add exporting assignments in assignment dao

--- assignment_dao.py
+++ assignment_dao.py
@@ -4,17 +4,22 @@ from assignment_model import Assignment
 
 
 class AssignmentDao():
-    assignment_file = 'assignments.csv'
+    assignments_file = 'assignments.csv'
 
     def create_assignment(self, email, name, url='', grade=''):
         return Assignment(email, name, url, grade)
 
     def import_assignments(self):
         assignments = []
-        with open(self.assignment_file, "r") as csvfile:
+        with open(self.assignments_file, "r") as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                print(row)
                 assignment = self.create_assignment(*row)
                 assignments.append(assignment)
         return assignments
+
+    def export_assignments(self, assignments):
+        with open(self.assignments_file, "w") as csvfile:
+            writer = csv.writer(csvfile)
+            for assignment in assignments:
+                writer.writerow(assignment.get_all_details())
