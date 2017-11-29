11776e08ab96c13c9d91046ebc10c018a4f11219
Joanna Baran <joannabaran56@gmail.com>
2017-11-29 11:59:40 +0100

    Implement assignment container

--- assignment_container.py
+++ assignment_container.py
@@ -1,2 +1,10 @@
 class AssignmentContainer():
-    pass
+
+    def __init__(self):
+        self.assignments = []
+
+    def get_assignment(self, index):
+        return self.assignments[index]
+
+    def add_assignment(self, assignment):
+        self.assignments.append(assignment)
