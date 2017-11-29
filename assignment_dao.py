4eb047bc67f1f0f4645bcd8f50970c4be74bd1e0
Joanna Baran <joannabaran56@gmail.com>
2017-11-29 11:38:38 +0100

    Add importing assignments in assignment dao

--- assignment_dao.py
+++ assignment_dao.py
@@ -1,8 +1,20 @@
+import csv
+
 from assignment_model import Assignment
 
 
 class AssignmentDao():
     assignment_file = 'assignments.csv'
 
-    def create_assignment(self, email, name, url=None, grade=None):
+    def create_assignment(self, email, name, url='', grade=''):
         return Assignment(email, name, url, grade)
+
+    def import_assignments(self):
+        assignments = []
+        with open(self.assignment_file, "r") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                print(row)
+                assignment = self.create_assignment(*row)
+                assignments.append(assignment)
+        return assignments
