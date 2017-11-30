0988704b87791ca80df767ffaca70407a5c30b50
Adrian Bala <aadrianbala@gmail.com>
2017-11-30 15:01:47 +0100

    Refactor get email and get password methods in user base view

--- user_base_view.py
+++ user_base_view.py
@@ -1,14 +1,19 @@
+import os
+
 from getpass import getpass
 
 
 class UserBaseView():
 
     def get_email(self):
+        os.system('clear')
         user_email = input('Enter e-mail: ')
         return user_email
 
     def get_password(self):
-        user_password = getpass('Enter password: ')
+        user_password = getpass('Enter password or E to exit: ')
+        if user_password.lower() == 'e':
+            exit()
         return user_password
 
     def display_text(self, text):
